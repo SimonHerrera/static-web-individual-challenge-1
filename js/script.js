31d742f7c679d442534d8a14d58c653b4db03924
Simon Herrera <simonherrera.tech@gmail.com>
2016-04-21 23:52:17 -0500

    Updated with Fibonacci

--- js/script.js
+++ js/script.js
@@ -18,3 +18,34 @@ function update2() {
 number.value = range.value  //so if range changes - make number = that range
 }
 
+// # 5 Fibonacci Number
+var input = document.getElementById('fab');
+input.addEventListener("change", grabNum);
+
+//Could do checks here OR just put var n = input.value and go right into var n = pass (all 1 function)
+function grabNum() {   //Why sometimes put (event)?
+    var pass = input.value
+
+    runFab(pass);
+    // console.log("any?", runfab );
+}
+
+function runFab(pass) {  //coming in, name within () can be called anything, just next line must match
+  var n = pass
+    console.log("num", n );
+
+  var answer = document.getElementById('ans');
+  var a = 0, b = 1, f = 1;
+  for(i = 2; i <= n; i++) {
+    f = a + b;
+    a = b;
+    b = f;
+  }
+
+        answer.innerHTML = "You Fibonacci Number is " + f;
+
+
+  // return f;
+  console.log("ans", f );
+}
+
